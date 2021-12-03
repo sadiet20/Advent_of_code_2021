db10bdea5fdd1d7e33fe80cc0f8a4954f77be77d
sadiet20 <sadierosethomas@gmail.com>
2021-12-02 22:26:51 -0800

    edited to reset file stream object to beginning

--- template/main.cpp
+++ template/main.cpp
@@ -10,6 +10,7 @@ unsigned long long part2(ifstream&);
 int main(){
 	ifstream finput;
 	ifstream ftest;
+	unsigned unsigned long output;
 
 	finput.open("input.txt");
 	ftest.open("test.txt");
@@ -18,27 +19,40 @@ int main(){
 		cout << "Error opening file" << endl;
 	}
 
-	unsigned long long output = part1(finput);
+	output = part1(finput);
 	cout << endl << "Part 1 sample input result: " << output << endl;
 	output = part1(ftest);
 	cout << "Part 1 test result: " << output << endl << endl;
+
+	//reset to beginning of files
+	finput.clear();
+	ftest.clear();
+	finput.seekg(0, ios::beg);
+	ftest.seekg(0, ios::beg);
 
 	output = part2(finput);
 	cout << "Part 2 sample input result: " << output << endl;
 	output = part2(ftest);
 	cout << "Part 2 test results: " << output << endl << endl;
 
+	finput.close();
+	ftest.close();
+
 	return 0;
 }
 
 
 unsigned long long part1(ifstream& fin){
-
+	while(!fin.eof()){
+	
+	}
 	return 0;
 }
 
 
 unsigned long long part2(ifstream& fin){
-
+	while(!fin.eof()){
+	
+	}
 	return 0;
 }
